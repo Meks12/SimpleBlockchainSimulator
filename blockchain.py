811517d1a2967afda9d65fcc4373b3016042d5b3
Meks <nikola.merlic2@gmail.com>
2023-12-28 14:15:20 +0100

    struktura blocka u blockchainu

---
+++ blockchain.py
@@ -0,0 +1,11 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.compute.hash()
