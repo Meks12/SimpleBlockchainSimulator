258320d41a4a86760d83c51e60712749d2d55e93
Meks <nikola.merlic2@gmail.com>
2023-12-28 14:25:44 +0100

    Metoda koja racuna hash blocka

--- blockchain.py
+++ blockchain.py
@@ -9,3 +9,14 @@ class Block:
         self.previous_hash = previous_hash
         self.nonce = nonce
         self.hash = self.compute.hash()
+    #Index - pozicija blocka u blockchainu
+    #Transactions - lista transakcija u blocku
+    #Timestamp = vrijeme kada je block napravljen
+    #Previous_hash = prijasnji block 
+    #nonce = vrijednost koja se koristi u miningu i POF
+    #self.hash = hash ovog blocka koji se racuna pomocu compute_hash
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.transactions}{self.timestamp}{self.previous_hash}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+    #Ova metoda racuna hash blocka
