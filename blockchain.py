5b0743dafb5ddc9176ed8c0564884316b03a6939
Meks <nikola.merlic2@gmail.com>
2023-12-28 23:32:09 +0100

    Validacija blockchaina - metoda

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 import hashlib
 import time
+import requests
+from urllib.parse import urlparse
 
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
@@ -24,6 +26,7 @@ class Block:
 class blockchain:
     def __init__(self):
         self.chain = []
+        self.nodes = set()
         self.create_genesis_block()
         #Inicijalizacija blockchaina sa praznom listom
         
@@ -39,3 +42,24 @@ class blockchain:
     def last_block(self):
         return self. chain[-1]
         #Vraca zadnji block u blockchainu
+    
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+    
+    def valid_chain(self, chain):
+        #Validacija Blockchaina
+        #Prvi block nakon genesisa
+        for i in range(1, len(chain)):
+            current_block = chain[i]
+            previous_block = chain[i - 1]
+
+            #Sadasnji hash blocka
+            if current_block['hash'] != self.hash_block(current_block):
+                return False
+
+            #Prijasnji hash blocka
+            if current_block['previous_hash'] != previous_block['hash']:
+                return False
+
+        return True
