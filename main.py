7958374a8a6c9e392441d131b028a1aeb6ea67ef
Meks <nikola.merlic2@gmail.com>
2024-01-04 19:27:26 +0100

    endpoint za dodavanje transakcija

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def get_chain():
     chain_data = [block.__dict__ for block in blockchain.chain]
     return {"length": len(chain_data), "chain": chain_data}
     #Dohvaca blockchain i vraca vrijednost bloka i duzinu blockchaina
+
+@app.post("/transactions/new")
+def add_transaction(transaction: dict):
+    # dodavanje transakcije u bc
+    return {"message": "Transaction will be added"}
