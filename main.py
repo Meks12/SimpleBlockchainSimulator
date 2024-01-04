273be4727028a22aa331dcf2e34bbd2a7bc18752
Meks <nikola.merlic2@gmail.com>
2024-01-04 23:29:50 +0100

    Brisanje nebitnog komentara

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-"""from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"} """
-
 from fastapi import FastAPI
 from blockchain import Blockchain
 from pydantic import BaseModel
